348f7f9c4beba7433a4b59e683157a8664d1ed28
PRODY (Juanma) <69425794+Juanma-venom@users.noreply.github.com>
2025-07-08 17:55:20 +0000

    TodoApp

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 const username = "Juanma-venom";
-const API_URL = `https://assets.breatheco.de/apis/fake/todos/user/${username}`;
+const API_URL = `https://playground.4geeks.com/todo/users/${username}`;
 
 const TodoApp = () => {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
 
   useEffect(() => {
-   fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+   fetch(`https://playground.4geeks.com/todo/users/${username}`, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
@@ -32,26 +32,26 @@ const TodoApp = () => {
 }, []);
 
   const getTasks = () => {
-    fetch(API_URL)
+    fetch(`https://playground.4geeks.com/todo/users/${username}`)
       .then((res) => res.json())
       .then((data) => {
-         console.log("Tareas obtenidas:", data); 
-        if (Array.isArray(data)) {
-          setTasks(data);
+        if (Array.isArray(data.todos)) {
+          setTasks(data.todos);
+          console.log(tasks)
         } else {
-          console.warn("Datos recibidos no son un array", data);
           setTasks([]);
         }
       })
       .catch((err) => console.error("Error al obtener tareas:", err));
   };
+  
 
   const addTask = (label) => {
     if (label.trim() === "") return;
 
     const newTask = { label: label.trim(), is_done: false };
 
-    fetch(API_URL, {
+    fetch(`https://playground.4geeks.com/todo/todos/${username}`, {
       method: "POST",
       body: JSON.stringify(newTask),
       headers: { "Content-Type": "application/json" },
@@ -67,7 +67,7 @@ const TodoApp = () => {
   };
 
   const deleteTask = (id) => {
-    fetch(`${API_URL}/${id}`, {
+    fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
       method: "DELETE",
     })
       .then((res) => {
@@ -82,7 +82,7 @@ const TodoApp = () => {
   const clearAllTasks = () => {
     Promise.all(
       tasks.map((task) =>
-        fetch(`${API_URL}/${task.id}`, {
+        fetch(`https://playground.4geeks.com/todo/todos/${task.id}`, {
           method: "DELETE",
         })
       )
@@ -90,6 +90,12 @@ const TodoApp = () => {
       .then(() => getTasks())
       .catch((err) => console.error("Error al limpiar tareas:", err));
   };
+
+  const listitem = tasks.map((task,index) =>
+    <li key={index} className="task-item d-flex p-3 ps-5 border-bottom">
+        {task.label} <button className="dltButton ms-auto" onClick={() => deleteTask(task.id)}>X</button>
+        </li>
+  )
 
   return (
     <div className="todo-container">
@@ -102,12 +108,7 @@ const TodoApp = () => {
         onKeyDown={(e) => e.key === "Enter" && addTask(input)}
       />
       <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            {task.label}
-            <button onClick={() => deleteTask(task.id)}>🗑</button>
-          </li>
-        ))}
+        {listitem}
       </ul>
       <button onClick={clearAllTasks}>Limpiar Todo</button>
     </div>
